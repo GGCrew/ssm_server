e7d2635ffa2ba99d6ee74b01abe978f5026e7083
Randy Connelie <R_Connelie@hotmail.com>
2014-09-11 08:14:06 +0000

    Rewrote migration for improved user feedback

--- db/migrate/20140903182258_add_sample_photos.rb
+++ db/migrate/20140903182258_add_sample_photos.rb
@@ -2,13 +2,22 @@ class AddSamplePhotos < ActiveRecord::Migration
   def change
 		reversible do |migration|
 			migration.up do
-				Photo.create!(:camera_id => 1, :date => Date.strptime('9-4-2014', '%m-%d-%Y'), :filename => 'IMG_4690.JPG')
-				Photo.create!(:camera_id => 1, :date => Date.strptime('9-4-2014', '%m-%d-%Y'), :filename => 'IMG_7159.JPG')
-				Photo.create!(:camera_id => 1, :date => Date.strptime('9-4-2014', '%m-%d-%Y'), :filename => 'IMG_7188.JPG')
-				Photo.create!(:camera_id => 1, :date => Date.strptime('9-4-2014', '%m-%d-%Y'), :filename => 'IMG_7627.JPG')
-				Photo.create!(:camera_id => 1, :date => Date.strptime('9-4-2014', '%m-%d-%Y'), :filename => 'IMG_7971.JPG')
-				Photo.create!(:camera_id => 1, :date => Date.strptime('9-4-2014', '%m-%d-%Y'), :filename => 'IMG_8673.JPG')
-				Photo.create!(:camera_id => 1, :date => Date.strptime('9-4-2014', '%m-%d-%Y'), :filename => 'IMG_8710.JPG')
+				photos = [
+					{:camera_id => 1, :date => '9-4-2014', :filename => 'IMG_4690.JPG'},
+					{:camera_id => 1, :date => '9-4-2014', :filename => 'IMG_7159.JPG'},
+					{:camera_id => 1, :date => '9-4-2014', :filename => 'IMG_7188.JPG'},
+					{:camera_id => 1, :date => '9-4-2014', :filename => 'IMG_7627.JPG'},
+					{:camera_id => 1, :date => '9-4-2014', :filename => 'IMG_7971.JPG'},
+					{:camera_id => 1, :date => '9-4-2014', :filename => 'IMG_8673.JPG'},
+					{:camera_id => 1, :date => '9-4-2014', :filename => 'IMG_8710.JPG'},
+				]
+				
+				say_with_time('Adding photos...') do
+					photos.each_with_index do |photo, index|
+						puts "#{index}/#{photos.count}"
+						Photo.create!(:camera_id => photo[:camera_id], :date => Date.strptime(photo[:date], '%m-%d-%Y'), :filename => photo[:filename])
+					end
+				end
 			end
 			
 			migration.down do
