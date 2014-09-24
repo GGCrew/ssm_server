c1342bdcff684df30a8a7e710f03c2f51836c71c
Randy Connelie <R_Connelie@hotmail.com>
2014-09-24 23:54:29 +0000

    Altered migration to forcefully set "play_state"

--- db/migrate/20140924000001_add_play_state_to_controls.rb
+++ db/migrate/20140924000001_add_play_state_to_controls.rb
@@ -1,5 +1,15 @@
 class AddPlayStateToControls < ActiveRecord::Migration
   def change
 		add_column	'controls',	'play_state',	:string
+
+		reversible do |migration|
+			migration.up do
+				ActiveRecord::Base.connection.execute("UPDATE controls SET play_state='play' WHERE play_state IS NULL;")
+			end
+			
+			migration.down do
+				#nothing needed
+			end
+		end
   end
 end
